ef1d73f75514fbc8d5716cd1245fc24f31873b28
Ion <ionglavan@mail.ru>
2018-03-23 01:15:26 +0200

    finished first part , section 19

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBYWRLQqDHmr_MfSe-irQ8qkDgBeB80K_M",
+    authDomain: "oshopv4.firebaseapp.com",
+    databaseURL: "https://oshopv4.firebaseio.com",
+    projectId: "oshopv4",
+    storageBucket: "",
+    messagingSenderId: "286618594105"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBYWRLQqDHmr_MfSe-irQ8qkDgBeB80K_M",
+    authDomain: "oshopv4.firebaseapp.com",
+    databaseURL: "https://oshopv4.firebaseio.com",
+    projectId: "oshopv4",
+    storageBucket: "",
+    messagingSenderId: "286618594105"
+  }
 };
